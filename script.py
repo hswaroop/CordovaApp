2384ca78565cb05b0179f0401ed59d02aaba084e
kratosjam1994 <himinspace@gmail.com>
2017-07-12 22:23:07 +0530

    Changed script.py and Book2.csv to a more generalized format

--- script.py
+++ script.py
@@ -2,61 +2,77 @@ import csv, os
 import xml.etree.ElementTree as ET
 import subprocess
 
+listanicon = ["ldpi", "mdpi", "hdpi", "xhdpi"]
+listansplashport = ["port-mdpi", "port-hdpi", "port-xhdpi", "port-xxhdpi", "port-xxxhdpi"] 
 f = open("Book2.csv")
 csvf = csv.reader(f)
+tree = ET.parse("config.xml")
+ET.register_namespace('', "http://www.w3.org/ns/widgets")
+ET.register_namespace('gap', "http://phonegap.com/ns/1.0")
+ET.register_namespace('cdv', "http://cordova.apache.org/ns/1.0")
+ET.register_namespace('android', "http://schemas.android.com/apk/res/android")
+root = tree.getroot()
+check = 0
 for row in csvf:
-    prev = os.getcwd()
-    os.chdir(row[0])
-    tree = ET.parse("config.xml")
-    ET.register_namespace('', "http://www.w3.org/ns/widgets")
-    ET.register_namespace('intelxdk', "http://xdk.intel.com/ns/v1")
-    root = tree.getroot()
-    root.find("{http://www.w3.org/ns/widgets}name").text = row[1]
-    root.find("{http://www.w3.org/ns/widgets}content").set("src", row[2]) 
-    path1 = row[3]
-    path2 = row[4]
-    dirs1 = os.listdir(path1)
-    dirs2 = os.listdir(path2)
-    platform = ET.SubElement(root, "platform")
-    platform.set("name", "android")
-    for files in dirs1:
-        icon = ET.SubElement(platform, "icon")
-    c=0
-    paths = []
-    for files in dirs1:
-        var = os.listdir(path1 + "/" + dirs1[c])
-        paths.append(path1 + "/" + dirs1[c] + "/" + var[0])
-        c = c+1
-    c=0
-    for icon in root.iter("icon"):
-        icon.set("src", paths[c])
-        c = c+1
-    for files in dirs2:
-        splash = ET.SubElement(platform, "splash")
-    c=0
-    paths1 = []
-    for files in dirs2:
-        var = os.listdir(path2 + "/" + dirs2[c])
-        paths1.append(path2 + "/" + dirs2[c] + "/" + var[0])
-        c = c+1
-    c=0
-    for splash in root.iter("splash"):
-        splash.set("src", paths1[c])
-        c =c+1
+    root.find("{http://www.w3.org/ns/widgets}name").text = row[0]
+    attribute = root.attrib
+    appid = attribute["id"]
+    root.set("id", appid + str(check))
+    if(check == 0):
+        platform = ET.SubElement(root, "platform")
+        platform.set("name", "android")
+        for var in range(1, 5):
+            icon = ET.SubElement(platform, "icon")
+            icon.set("density", listanicon[var-1])
+            icon.set("src", row[var])
+        for var in range(5, 10):
+            splash = ET.SubElement(platform, "splash")
+            splash.set("density", listansplashport[var-5])
+            splash.set("src", row[var])
+    else:
+        reset = 1
+        for icon in root.iter("icon"):
+            icon.set("src", row[reset])
+            reset = reset + 1
+        for splash in root.iter("splash"):
+            splash.set("src", row[reset])
+
     tree.write("config.xml")
-    with open(row[5], "r") as rf:
-        data = rf.read()
-        data = data.replace(row[11], row[12])
-    with open(row[5], "w") as wf:
-        wf.write(data)
-    with open(row[6], "r") as rf:
-        data = rf.read()
-        data = data.replace(row[7], row[8])
-        data = data.replace("../images" + "/" + row[9], "../images" + "/" + row[10])
-    with open(row[6], "w") as wf:
-        wf.write(data)  
-    subprocess.call("cordova build", shell = True)
-    os.chdir(prev)
-f.close()               
-                
-     
+    
+    with open(row[10], "r+") as rf:
+        listfiles = rf.readlines()
+        length = len(listfiles)
+        for var in range(0,length):
+            var1 = listfiles[var].find(row[11])
+            if(var1!=-1):
+                listfiles[var] = "var" + " " + row[11] + " " + "=" + " " + row[12] + ";" + "\n"
+                break    
+        rf.seek(0,0)
+        for lines in listfiles:
+            rf.writelines(lines)
+        
+    with open(row[13], "r+") as rf:
+        listfiles = rf.readlines()
+        length = len(listfiles)
+        flag1=0
+        flag2=0
+        for var in range(0,length):
+            var1 = listfiles[var].find(row[14])
+            var2 = listfiles[var].find(row[16])
+            if(var1!=-1):
+                listfiles[var] = "--" + row[14] + ":" + " " + row[15] + ";" + "\n"
+                flag1=1
+            elif(var2!=-1):
+                listfiles[var] = "--" + row[16] + ":" + " " + row[17] + ";" + "\n"
+                flag2=1
+            if(flag1==1 and flag2==1):
+                break
+        rf.seek(0,0)
+        for lines in listfiles:
+            rf.writelines(lines)
+            
+    subprocess.call("cordova build", shell = "True")
+    check = check + 1
+    os.rename("platforms/android/build/outputs/apk/android-debug.apk", "platforms/android/build/outputs/apk/android-debug" + str(check) + ".apk")
+    #os.rename("platforms/android/build/outputs/apk/android-debug" + str(check) + ".apk", "platforms/android-debug" + str(check) + ".apk")
+f.close()        
